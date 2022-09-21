a37559e79b0a7ceb19a495c5a80d785451093bbf
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-09-21 19:39:48 +0400

    fixed ordinalStatistics

--- OrdinalStatistics.cpp
+++ OrdinalStatistics.cpp
@@ -1,60 +1,60 @@
-﻿#include <iostream>
-#include <vector>
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <random>
 #include <algorithm>
+#define endl '\n'
 using namespace std;
-void printArray(unsigned long long* array, int n)
-{
-    for (int i = 0; i < n; ++i)
-        cout << array[i] << " ";
-    cout << endl;
-}
-void quickSort(unsigned long long* array, int low, int high)
-{
-    int i = low;
-    int j = high;
-    int pivot = array[(i + j) / 2];
-    int temp;
+typedef long long ll;
+const int MOD = 1e9 + 7;
+int arr[(int)4e7 + 10], k, n;
 
-    while (i <= j)
-    {
-        while (array[i] < pivot) {
-            i++;
-        }
-        while (array[j] > pivot) {
-            j--;
-        }
-        if (i <= j)
-        {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-            j--;
-        }
-    }
-    if (j > low) {
-        quickSort(array, low, j);
-    }
-    if (i < high) {
-        quickSort(array, i, high);
-    }
+int m_rand(int l, int r) {
+    return 1.0 * rand() / RAND_MAX * (r - l) + l;
 }
 
-int main()
-{
-    int n, x, k;
-    unsigned long long y;
+void quick_sort(int* ar, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+    int x = m_rand(left, right);
+    swap(ar[left], ar[x]);
+    x = ar[left];
+    int li = left + 1, ri = right;
+    while (true) {
+        while (ar[li] < x && li < right) {
+            ++li;
+        }
+        while (ar[ri] > x && ri > left) {
+            --ri;
+        }
+        if (li >= ri) {
+            break;
+        }
+        swap(ar[li], ar[ri]);
+        ++li;
+        --ri;
+    }
+    swap(ar[left], ar[ri]);
+    if (k < ri) {
+        quick_sort(ar, left, ri - 1);
+    } else if(k > ri) {
+        quick_sort(ar, ri + 1, right);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    long long x, y;
     cin >> n;
-    unsigned long long arr[100000];
+    int* arr = new int[n];
     cin >> arr[0] >> x >> y;
-    cin >> k;
-    unsigned long long mod = (pow(10, 9) + 7);
     for (int i = 1; i < n; i++) {
-        arr[i] = (arr[i - 1] * x + y) % mod;
+        arr[i] = (arr[static_cast<long long>(i) - 1] * x + y) % MOD;
     }
-    printArray(arr, n);
-    quickSort(arr, 0, n);
-    printArray(arr, n);
+    cin >> k;
+    quick_sort(arr, 0, n - 1);
     cout << arr[k];
-    return (0);
+    return 0;
 }
