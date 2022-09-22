25801511c51934fb9c6e5ed0decc25e8b8d7186f
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-09-23 00:55:00 +0400

    4 solves secondBlock

---
+++ SecondBlock/Everyone loves equations.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cin >> m;
+    int* x = new int[m];
+    for (int i = 0; i < m; i++) {
+        cin >> x[i];
+    }
+    int sum = 0;
+    int* p;
+    int i = 0;
+    while (i != m) {
+        p = find(arr, arr + n, x[i]);
+        if (p != arr + n) {
+            sum += distance(arr, p);
+        }
+        else {
+            sum--;
+        }
+        i++;
+    }
+    cout << sum;
+    return 0;
+}
